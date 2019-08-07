6317d0c5140f398251abb0441011cca8b1004f90
gregorysysoev <gregorysysoev@gmail.com>
2019-08-07 16:11:39 +1000

    before change IWriter and logic of print

---
+++ myTree/Writer/FileWriter.cs
@@ -0,0 +1,25 @@
+using myTree;
+using System;
+using System.Collections.Generic;
+
+namespace myTree
+{
+    public class FileWriter : IWriter
+    {
+        private List<string> _listOfFiles = new List<string>();
+        public void Write(string text)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void WriteLine()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void WriteLine(string text)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
